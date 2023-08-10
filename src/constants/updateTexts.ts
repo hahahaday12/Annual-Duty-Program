2c47acaecee989d6f2166b02a6d01c5c4e5b0fac
hookor <gerrard1219@gmail.com>
2023-08-10 15:20:53 +0900

    fix: updateform failure message update

--- src/constants/updateTexts.ts
+++ src/constants/updateTexts.ts
@@ -14,7 +14,8 @@ export const UpdateTexts = {
   newPwdPh: '영어 대문자, 영어 소문자, 숫자, 특수문자를 모두 포함 (8글자 이상)',
   newPwdCheckPh: '새로운 비밀번호를 한번 더 입력해주세요.',
   verification: '새로운 비밀번호와 비밀번호 확인을 동일하게 입력해주세요.',
-  success: '회원정보가 수정되었습니다. 변경된 비밀번호로 로그인해주세요.'
+  success: '회원정보가 수정되었습니다. 변경된 비밀번호로 로그인해주세요.',
+  failure: '변경할 비밀번호를 조건에 맞게 입력해주세요.'
 }
 
 export const nameTexts = [
