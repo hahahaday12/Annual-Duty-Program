c9de6588130a17e5b03a211aef2be8634eb3bed2
hahahaday12 <hahahaday12@naver.com>
2023-08-10 16:39:46 +0900

    fix: delete module

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,14 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from 'pages/Router'
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'styles/index'
-import { QueryClient, QueryClientProvider } from "react-query";
 
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
-  <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-      </QueryClientProvider>
-  </ThemeProvider>
+    <ThemeProvider theme={theme}>
+          <RouterProvider router={router} />
+    </ThemeProvider>
+
 )
 
 
