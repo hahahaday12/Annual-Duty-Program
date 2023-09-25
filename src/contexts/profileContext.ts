ed45db551d1b9f4b67d007d1441d5b5e4621816e
hahahaday12 <hahahaday12@naver.com>
2023-09-26 01:40:56 +0900

    delete: 사용하지 않는 context 파일 삭제

--- src/contexts/profileContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-type ProfileState = {
-  profileImage: string
-  setProfileImage: (value: string) => void
-}
-
-export const ProfileContext = createContext<ProfileState>({} as ProfileState)
