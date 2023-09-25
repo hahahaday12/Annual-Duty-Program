0cd13e326feb8c0a9435dd7816b905c979303dfd
hahahaday12 <hahahaday12@naver.com>
2023-09-26 01:32:22 +0900

    Chore: recoil root 설정

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import { RouterProvider } from 'react-router-dom'
 import { router } from 'pages/Router'
 import { ThemeProvider } from 'styled-components'
 import { theme } from 'styles/index'
-
+import { RecoilRoot } from 'recoil'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-
+  <RecoilRoot>
     <ThemeProvider theme={theme}>
-          <RouterProvider router={router} />
+      <RouterProvider router={router} />
     </ThemeProvider>
-
+  </RecoilRoot>
 )
-
-
-
